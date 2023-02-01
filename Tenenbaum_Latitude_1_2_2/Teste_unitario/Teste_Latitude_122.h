b68ba0531d782e1570ae985672c311f074940112
Renato de Pierri <renatopierri@users.noreply.github.com>
2023-02-01 17:08:25 -0300

    Pequenas alterações
    Pequenas alterações

--- Tenenbaum_Latitude_1_2_2/Teste_unitario/Teste_Latitude_122.h
+++ Tenenbaum_Latitude_1_2_2/Teste_unitario/Teste_Latitude_122.h
@@ -4,8 +4,17 @@
 #include "CuTest.h"
 CuSuite*StrUtilGetSuite();
 void RunAllTests(void);
-
-//Testes unitarios
-//void Teste_Gera_Aleatorio(CuTest *tc);
+void Teste_Le_Dados_automatico(CuTest *tc);
+void Teste_Le_Dados_manual(CuTest *tc);
+void Teste_Classifica_Dados(CuTest *tc);
+void Teste_Remove_Inconsistencias1(CuTest *tc);
+void Teste_Remove_Inconsistencias2(CuTest *tc);
+void Teste_Remove_Inconsistencias3(CuTest *tc);
+void Teste_Remove_Inconsistencias4(CuTest *tc);
+void Teste_Remove_Inconsistencias5(CuTest *tc);
+void Teste_Remove_Inconsistencias6(CuTest *tc);
+void Teste_Remove_Inconsistencias7(CuTest *tc);
+void Teste_Calcula_Media1(CuTest *tc);
+void Teste_print_erro1(CuTest *tc);
 
 #endif // TESTE_LATITUDE_122_H_INCLUDED
